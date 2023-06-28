b87eb2fb106dec6f14746d3478c7b9f17d3edd6c
Nahuel <nahuel.profesor@gmail.com>
2023-06-27 23:40:52 -0300

    relación muchos a muchos agregada

---
+++ muchos_a_muchos.py
@@ -0,0 +1,70 @@
+import peewee
+
+database = peewee.MySQLDatabase('pythonpeewee', 
+    host='localhost', 
+    port=3306, 
+    user='root', 
+    passwd='password')
+
+
+class Product(peewee.Model):
+    title = peewee.CharField(max_length=50)
+    price = peewee.DecimalField(max_digits=10, decimal_places=2)
+    
+    class Meta:
+        database = database
+        db_table = 'products'
+        
+    def __str__(self):
+        return self.title        
+
+class Category(peewee.Modelo):
+    title = peewee.CharField(max_length=20)
+    
+    class Meta:
+        database = database
+        db_table = 'categories'
+        
+    def __str__(self):
+        return self.title
+
+class ProductCategory(peewee.Model):
+    product = peewee.ForeignKeyField(Product, backref='categories')
+    category = peewee.ForeignKeyField(Category, backref='products') 
+    
+    class Meta:
+        database = database
+        db_table = 'product_categories'          
+    
+if __name__ == '__main__':
+    
+    database.drop_tables([Product, Category, ProductCategory])
+    database.create_tables([Product, Category, ProductCategory])
+    
+    ipad = Product.create(title='Ipad', price=500.50)
+    iphone = Product.create(title='Iphone', price=800.00)
+    tv = Product.create(title='TV', price=600.00)
+    
+    technology = Category.create(title='Technology')
+    home = Category.create(title='Home')
+    
+    ProductCategory.create(product=ipad, category=technology)
+    ProductCategory.create(product=iphone, category=technology)
+    ProductCategory.create(product=tv, category=technology)
+    
+    ProductCategory.create(product=tv, category=home)
+    
+    # for product_category in technology.products: 
+    #     print(product_category.product)    
+        
+    for product_category in tv.categories:
+        print(product_category.category)
+        
+            
+    
+    
+    
+    
+    
+    
+
